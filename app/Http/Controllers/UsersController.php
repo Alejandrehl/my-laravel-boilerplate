43ff75a807adc640100d8e837ff8e3875fb35a8c
Alejandro <alejandrehl@icloud.com>
2019-03-03 03:10:20 -0300

    Avance en middleware

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,7 +9,7 @@ class UsersController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware(['auth', 'roles']);
     }
 
     /**
