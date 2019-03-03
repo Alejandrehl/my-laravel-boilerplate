d1e2f1f7eb6b1d342d6f5270435e2df4b96a5f7f
Alejandro <alejandrehl@icloud.com>
2019-03-03 02:19:10 -0300

    Mejora en usuarios y navegación

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,6 +58,12 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    @if(Auth::user()->role === "admin")
+                                        <a class="nav-link" href="{{ route('users.index') }}">
+                                            <i class="fas fa-users"></i>
+                                            {{ __('Users') }}
+                                        </a>
+                                    @endif
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
