ddccad2b4e2ad4d201d89e7b9dc0accad62d0b9d
Alejandro <alejandrehl@icloud.com>
2019-03-03 02:51:15 -0300

    Index de usuarios

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,4 +1,26 @@
 @extends('layouts.app')
 @section('content')
     <h1>@lang('User List')</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>@lang('Name')</th>
+                <th>@lang('E-Mail Address')</th>
+                <th>@lang('Role')</th>
+                <th>@lang('Actions')</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($users as $user)
+            <tr>
+                <td>{{ $user->id }}</td>
+                <td>{{ $user->name }}</td>
+                <td>{{ $user->email }}</td>
+                <td>{{ $user->role }}</td>
+                <td></td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
